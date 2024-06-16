c72816e88a1348ef7f69713e5840447fc4bf5d0c
yessenia22c <65354520+yessenia22c@users.noreply.github.com>
2024-06-16 18:03:52 -0400

    feat: Add API service for fetching reservations by dates

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -3,13 +3,17 @@
 
 import 'dart:collection';
 
+import 'package:app_reservas/api/reservas_por_fechas.dart';
+import 'package:app_reservas/main.dart';
+import 'package:flutter/widgets.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 /// Example event class.
 class Event {
   final String title;
+  final String description;
 
-  const Event(this.title);
+  const Event(this.title, this.description);
 
   @override
   String toString() => title;
@@ -18,21 +22,24 @@ class Event {
 /// Example events.
 ///
 /// Using a [LinkedHashMap] is highly recommended if you decide to use a map.
+///
+//obtener las reserva de la pticion y mandarsela al KEnvents
+
 final kEvents = LinkedHashMap<DateTime, List<Event>>(
   equals: isSameDay,
   hashCode: getHashCode,
-)..addAll(_kEventSource);
+);
 
-final _kEventSource = Map.fromIterable(List.generate(50, (index) => index),
-    key: (item) => DateTime.utc(kFirstDay.year, kFirstDay.month, item * 5),
-    value: (item) => List.generate(
-        item % 4 + 1, (index) => Event('Event $item | ${index + 1}')))
-  ..addAll({
-    kToday: [
-      Event('Today\'s Event 1'),
-      Event('Today\'s Event 2'),
-    ],
-  });
+// final _kEventSource = Map.fromIterable(List.generate(50, (index) => index),
+//     key: (item) => DateTime.utc(kFirstDay.year, kFirstDay.month, item * 5),
+//     value: (item) => List.generate(
+//         item % 4 + 1, (index) => Event('Event $item | ${index + 1}')))
+//   ..addAll({
+//     kToday: [
+//       Event('Today\'s Event 1'),
+//       Event('Today\'s Event 2'),
+//     ],
+//   });
 
 int getHashCode(DateTime key) {
   return key.day * 1000000 + key.month * 10000 + key.year;
@@ -50,3 +57,16 @@ List<DateTime> daysInRange(DateTime first, DateTime last) {
 final kToday = DateTime.now();
 final kFirstDay = DateTime(kToday.year, kToday.month - 3, kToday.day);
 final kLastDay = DateTime(kToday.year, kToday.month + 3, kToday.day);
+
+Future<void> cargarReservasApi() async {
+  try {
+    final apiService = ApiService();
+    Map<DateTime, List<Event>> reservas = await apiService.reservasPorFechas();
+    print('LLEGA AQUI $reservas');
+    print(reservas);
+
+    kEvents.addAll(reservas);
+  } catch (e) {
+    print(' Error al obtener los eventos de reservas $e');
+  }
+}
